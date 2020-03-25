8acacf1b8580c9d5778fee382b73f8dce050e6e1
Vini Salazar <viniws@gmail.com>
2020-03-24 23:24:02 -0300

    Add Universal tree command

--- data/gtt/gtt_cmd.sh
+++ data/gtt/gtt_cmd.sh
@@ -1,3 +1,4 @@
-GToTree -A gtt_input.csv -H Cyanobacteria -o gtt_cyanobacteria -m gtt_labels.tsv -n 64 -j 64
-GToTree -A gtt_input.csv -H Bacteria -o gtt_bacteria -m gtt_labels.tsv -n 64 -j 64
+# GToTree -A gtt_input.csv -H Cyanobacteria -o gtt_cyanobacteria -m gtt_labels.tsv -n 64 -j 64
+# GToTree -A gtt_input.csv -H Bacteria -o gtt_bacteria -m gtt_labels.tsv -n 64 -j 64
+GToTree -A gtt_input.csv -H Universal_Hug_et_al -o gtt_universal -m gtt_labels.tsv -n 64 -j 64
 
